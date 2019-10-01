1b7e6bcbb9f1bf0f4d1e675740a23acab5342e35
ywang4 <ywang4@linkedin.com>
2019-10-01 15:55:23 -0700

    update

--- helix-core/src/test/java/org/apache/helix/experiment/RebalanceAlgorithmAnalysis.java
+++ helix-core/src/test/java/org/apache/helix/experiment/RebalanceAlgorithmAnalysis.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -21,6 +22,7 @@ import org.apache.helix.model.ResourceAssignment;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
+
 public class RebalanceAlgorithmAnalysis {
   private static List<Float> randomGenerateConfigs(int size) {
     List<Float> result = new ArrayList<>();
@@ -31,8 +33,7 @@ public class RebalanceAlgorithmAnalysis {
     return result;
   }
 
-  private static void writeToCSV(String fileName, List<String> columns, List<List<String>> rows)
-      throws IOException {
+  private static void writeToCSV(String fileName, List<String> columns, List<List<String>> rows) throws IOException {
 
     FileWriter csvWriter = new FileWriter(fileName);
     csvWriter.append(String.join(",", columns)).append("\n");
@@ -51,25 +52,25 @@ public class RebalanceAlgorithmAnalysis {
     return r;
   }
 
-  private static double[] simulate(RebalanceAlgorithm rebalanceAlgorithm,
-      MockClusterModel clusterModel) throws HelixRebalanceException {
-    float totalPartitionsCount = clusterModel.getContext().getAllReplicas().size();
-    Map<String, ResourceAssignment> initPossibleAssignment =
-        clusterModel.getContext().getBestPossibleAssignment();
+  private static Map<String, Double> simulate(RebalanceAlgorithm rebalanceAlgorithm, MockClusterModel clusterModel)
+      throws HelixRebalanceException {
+    Map<String, Double> stats = new HashMap<>();
+    Map<String, ResourceAssignment> initPossibleAssignment = clusterModel.getContext().getBestPossibleAssignment();
     OptimalAssignment optimalAssignment = rebalanceAlgorithm.calculate(clusterModel);
-    double evenness = clusterModel.getCoefficientOfVariation().get("size");
-    double partitionMovements =
-        clusterModel.getTotalMovedPartitionsCount(optimalAssignment, initPossibleAssignment)
-            / totalPartitionsCount;
+    double stdEvenness = clusterModel.getCoefficientOfVariation().get("size");
+    stats.put("stdEvenness", stdEvenness);
+    double partitionMovements = clusterModel.getTotalMovedPartitionsCount(optimalAssignment, initPossibleAssignment);
+    stats.put("partitionMovement", partitionMovements);
     double maxResourceCount = clusterModel.getMaxResourceCountAsEvenness();
-    double maxPartitionsCount = clusterModel.getMaxPartitionsCountAsEvenness();
-    double maxUsage = clusterModel.getMaxCapacityKeyUsageAsEvenness();
+    stats.put("maxResourceCount", maxResourceCount);
+    double maxPartitionCount = clusterModel.getMaxPartitionsCountAsEvenness();
+    stats.put("maxPartitionCount", maxPartitionCount);
+    double maxCapacityKeyUsage = clusterModel.getMaxCapacityKeyUsageAsEvenness();
+    stats.put("maxCapacityKeyUsage", maxCapacityKeyUsage);
     double maxTopStateCount = clusterModel.getMaxTopStatesAsEvenness();
+    stats.put("maxTopStateUsage", maxTopStateCount);
 
-    return new double[] {
-        evenness, partitionMovements, maxResourceCount, maxPartitionsCount, maxUsage,
-        maxTopStateCount
-    };
+    return stats;
   }
 
   private static MockClusterModel reset(MockClusterModel baseModel) {
@@ -83,8 +84,11 @@ public class RebalanceAlgorithmAnalysis {
 
   public static void main(String[] args) throws HelixRebalanceException, IOException {
     MockClusterModel baseModel = new MockClusterModelBuilder("TestCluster").setZoneCount(4)
-        .setInstanceCountPerZone(10).setResourceCount(3).setPartitionCountPerResource(15)
-        .setMaxPartitionsPerInstance(10).build();
+        .setInstanceCountPerZone(10)
+        .setResourceCount(3)
+        .setPartitionCountPerResource(15)
+        .setMaxPartitionsPerInstance(10)
+        .build();
 
     List<List<String>> result = new ArrayList<>();
 
@@ -94,26 +98,22 @@ public class RebalanceAlgorithmAnalysis {
       float[] randomWeights = getPrimitives(numbers);
       RebalanceAlgorithm algorithm = getAlgorithm(randomWeights);
       OptimalAssignment initAssignment = algorithm.calculate(clusterModel);
-      Map<String, ResourceAssignment> initBestAssignment =
-          initAssignment.getOptimalResourceAssignment();
+      Map<String, ResourceAssignment> initBestAssignment = initAssignment.getOptimalResourceAssignment();
       clusterModel.getContext().setBestPossibleAssignment(initBestAssignment);
       clusterModel.getContext().setBaselineAssignment(initBestAssignment);
       // create a list of new nodes
-      List<AssignableNode> newNodes = MockClusterModelBuilder.createInstances("NewInstance", 40,
-          "NewZone", ImmutableMap.of("size", 1000), 30);
+      List<AssignableNode> newNodes =
+          MockClusterModelBuilder.createInstances("NewInstance", 40, "NewZone", ImmutableMap.of("size", 1000), 30);
       // add these new nodes to the cluster
       clusterModel.onClusterExpansion(newNodes);
 
-      double[] stats = simulate(algorithm, clusterModel);
-      numbers.add((float) stats[0]);
-      numbers.add((float) stats[1]);
-
+      Map<String, Double> stats = simulate(algorithm, clusterModel);
+      //TODO: collect stats
       result.add(numbers.stream().map(String::valueOf).collect(Collectors.toList()));
     }
 
-    List<String> names =
-        ImmutableList.of("PartitionMovement", "InstancePartitionCount", "ResourcePartitionCount",
-            "ResourceTopStateCount", "MaxCapacityKeyUsage", "evenness", "movements");
+    List<String> names = ImmutableList.of("PartitionMovement", "InstancePartitionCount", "ResourcePartitionCount",
+        "ResourceTopStateCount", "MaxCapacityKeyUsage", "evenness", "movements");
     writeToCSV("dataset-40.csv", names, result);
   }
 }
