674dc475083d050279775436eb57ee65a67b11c2
ywang4 <ywang4@linkedin.com>
2020-02-03 15:05:56 -0800

    Experiment zk large znode issues

--- helix-core/src/main/java/org/apache/helix/controller/ZkClientMain.java
+++ helix-core/src/main/java/org/apache/helix/controller/ZkClientMain.java
@@ -1,24 +1,5 @@
 package org.apache.helix.controller;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.locks.Condition;
@@ -32,19 +13,21 @@ import org.apache.helix.manager.zk.ZkBaseDataAccessor;
 import org.apache.helix.manager.zk.ZkClient;
 import org.apache.helix.manager.zk.client.DedicatedZkClientFactory;
 import org.apache.helix.manager.zk.client.HelixZkClient;
+import org.apache.zookeeper.data.Stat;
 import org.testng.annotations.Test;
 
 
 public class ZkClientMain {
-  private static final String LOCAL_ZK = "localhost:2121";
-  private static final String LARGE_NODE_PATH = "/test1MB";
+  private static final String LOCAL_ZK = "localhost:2183";
+  private static final String LARGE_NODE_PATH = "/test2MB";
 
   @Test
   public void testCreateLargeZnode() {
-//    System.setProperty("jute.maxbuffer", String.valueOf(1024 * 1024 * 2L));
-    System.setProperty("jute.maxbuffer", 4096 * 1024 * 10 + "");
-    byte[] bytes = new byte[1024 * 1024];
+    System.setProperty("jute.maxbuffer", String.valueOf(1024 * 1024 * 5L));
+    byte[] bytes = new byte[1024 * 1023];
     Arrays.fill(bytes, (byte) 1);
+    String maxBufferString = System.getProperty("jute.maxbuffer");
+    System.out.println(maxBufferString);
     ZkClient zkClient = new ZkClient(LOCAL_ZK);
     try {
       System.out.println("Attempt to create znode size: " + bytes.length);
@@ -59,6 +42,8 @@ public class ZkClientMain {
   @Test
   public void testReadLargeZnode() {
     System.setProperty("jute.maxbuffer", String.valueOf(1024 * 1024));
+    String maxBufferString = System.getProperty("jute.maxbuffer");
+    System.out.println(maxBufferString);
     ZkSerializer zkSerializer = new ZkSerializer() {
       @Override
       public byte[] serialize(Object o) throws ZkMarshallingError {
@@ -75,53 +60,13 @@ public class ZkClientMain {
         new HelixZkClient.ZkConnectionConfig(LOCAL_ZK),
         new HelixZkClient.ZkClientConfig().setZkSerializer(zkSerializer));
     try {
+//      Stat stat = zkClient.getStat(LARGE_NODE_PATH);
       Object data = zkClient.readData(LARGE_NODE_PATH);
+      System.out.println("Read the data content: " + data);
     } catch (Exception e) {
       System.out.println("Error while creating the node" + e.getMessage());
     } finally {
-      zkClient.delete(LARGE_NODE_PATH);
-      zkClient.close();
-    }
-  }
-
-  @Test
-  public void test() {
-    ZkClient zkClient = new ZkClient("localhost:2121");
-    Lock lock = new ReentrantLock();
-    Condition condition = lock.newCondition();
-
-//    zkClient.subscribeDataChanges("/ABC/TEST", new IZkDataListener() {
-//      @Override
-//      public void handleDataChange(String s, Object o)
-//          throws Exception {
-//        System.out.println("Data Change: " + s);
-//      }
-//
-//      @Override
-//      public void handleDataDeleted(String s)
-//          throws Exception {
-//        System.out.println("Data deleted: " + s);
-//      }
-//    });
-
-    zkClient.subscribeChildChanges("/ABC", new IZkChildListener() {
-      @Override
-      public void handleChildChange(String s, List<String> list)
-          throws Exception {
-        System.out.println("Path of child change: " + s);
-        System.out.println("Child changes: " + list);
-      }
-    });
-
-    try {
-      lock.lock();
-      System.out.println("Wait for condition");
-      condition.await();
-      lock.unlock();
-      System.out.println("Condition meet");
-    } catch (InterruptedException e) {
-
-    } finally {
+//      zkClient.delete(LARGE_NODE_PATH);
       zkClient.close();
     }
   }
